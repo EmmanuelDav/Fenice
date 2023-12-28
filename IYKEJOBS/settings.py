03375037a8837eb3534dee7753de3992b5d0af27
emmanueldav <kuradavis987@gmail.com>
2023-12-28 16:51:45 +0100

    migration complete

--- IYKEJOBS/settings.py
+++ IYKEJOBS/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'IYKEJOBS.urls'
